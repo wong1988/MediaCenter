3c03c62c1cae1afc30f946a976442f75829323e9
wong1988 <wong1988@petalmail.com>
2022-06-22 14:52:55 +0800

    0.0.1

--- app/src/main/java/io/github/media/example/MainActivity.java
+++ app/src/main/java/io/github/media/example/MainActivity.java
@@ -1,8 +1,8 @@
 package io.github.media.example;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
